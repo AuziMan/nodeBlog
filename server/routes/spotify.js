65d8908924e11714c04e2a387e8d37b1b077a29b
AuziMan <austintdriver@gmail.com>
2024-06-13 10:51:09 -0700

    adding now Playing section

--- server/routes/spotify.js
+++ server/routes/spotify.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getTopTracks } = require('../config/spotify'); // Adjust the path accordingly
+const { getTopTracks, getNowPlaying } = require('../config/spotify'); // Adjust the path accordingly
 
 /**
  * GET /songs
@@ -15,5 +15,49 @@ router.get('/songs', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+
+// router.get('/nowPlaying', async (req, res) => {
+//   try {
+//     const response = await getNowPlaying();
+//     //console.log('Raw Response:', response);  // Log raw response
+
+//     // Check response status
+//     if (!response || response.status === 204 || response.status > 400) {
+//       return res.status(200).json({ isPlaying: false });
+//     }
+
+//     // Parse JSON only if the response is okay
+//     const song = await response.json();
+
+//     // Check if the song object has the expected structure
+//     if (!song || !song.item) {
+//       return res.status(200).json({ isPlaying: false });
+//     }
+
+//     const isPlaying = song.is_playing;
+//     const title = song.item.name;
+//     const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
+//     const album = song.item.album.name;
+//     const albumImageUrl = song.item.album.images[0].url;
+//     const songUrl = song.item.external_urls.spotify;
+
+//     res.setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=30');
+
+//     return res.status(200).json({
+//       album,
+//       albumImageUrl,
+//       artist,
+//       isPlaying,
+//       songUrl,
+//       title,
+//     });
+//   } catch (error) {
+//     console.error('Error fetching now playing track:', error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
+
+
 
 module.exports = router;
